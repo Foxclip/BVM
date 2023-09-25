b73dab778769b945a6a3d97dcc97683ab88a52bc
Foxclip <destroyApp2@gmail.com>
2023-09-25 23:20:23 +1000

    Add repl test

--- main.cpp
+++ main.cpp
@@ -26,7 +26,6 @@ int main() {
 		std::cout << "EXCEPTION: " << exc.what() << "\n";
 	}
 
-	// TODO: repl instruction, replaces one subtree with another
 	// TODO: functions can return pointers
 	// TODO: replace command string tokens in the code with enum values
 	// TODO: shift pointers from pointer list, instead of scanning the whole token list
@@ -43,7 +42,7 @@ int main() {
 	// TODO: file instruction that reads file and converts it into a list of chars
 	// TODO: malloc, free and read instructions for working with heap memory
 	// TODO: getaddr instruction, get absolute address of the current node
-	// TODO: modifying instructions can only address stuff inside its list
+	// TODO: modifying instructions can only address stuff inside its list (block instruction?)
 	// TODO: fractal lists?
 	// TODO: parallel computation of lists and CUDA version
 
