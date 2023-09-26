182f8362ac6946206b58aee53a779733d96b2ab3
Foxclip <destroyApp2@gmail.com>
2023-09-27 01:31:22 +1000

    Add factorial test

--- main.cpp
+++ main.cpp
@@ -26,7 +26,8 @@ int main() {
 		std::cout << "EXCEPTION: " << exc.what() << "\n";
 	}
 
-	// TODO: repl executes its second argument, also add some instruction to stop code from executing (or to force execute the code?)
+	// TODO: adding number to pointer test
+	// TODO: repl executes its second argument, also add code instruction to stop code from executing
 	// TODO: block instruction, separates program into blocks, blocks have
 	// TODO: functions can return pointers
 	// TODO: replace command string tokens in the code with enum values
